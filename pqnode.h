3f65b371916a8bce207c1f58cd03771b1e7f9921
arthurbright <artb1234567@gmail.com>
2024-12-26 18:44:40 -0500

    add defaults for fields

--- pqnode.h
+++ pqnode.h
@@ -12,9 +12,8 @@ class PQNode
   public:
     PQNode_types type;
     T value; // if this is a leaf node
-    PQNode<T>* parent;
-    vector<PQNode<T>*> children;
-    bool blocked;
+    PQNode<T>* parent = nullptr;
+    vector<PQNode<T>*> children = {};
 
 // constructors
     PQNode(PQNode_types type);
--- pqtree.h
+++ pqtree.h
@@ -11,7 +11,7 @@ template <typename T>
 class PQTree {
   public:
     vector<PQNode<T>*> leaves;
-    PQNode<T>* root;
+    PQNode<T>* root = nullptr;
 // constructors
     // by default, the starting node in a PQ tree is a pnode
     PQTree(const vector<T>& universe);
